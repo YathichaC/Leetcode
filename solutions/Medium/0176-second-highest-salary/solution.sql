759ac60014bb6118d4a421ee742953d3ee1d3b5e
YathichaC <yathichac@users.noreply.github.com>
2026-09-22 06:42:43 +0000

    leetie: Add 176. Second Highest Salary [Medium] (mysql)

---
+++ solutions/Medium/0176-second-highest-salary/solution.sql
@@ -0,0 +1,18 @@
+-- ──────────────────────────────────────────────────
+-- Problem  : 176. Second Highest Salary
+-- Difficulty: Medium
+-- Tags     : Database
+-- Link     : https://leetcode.com/problems/second-highest-salary/
+-- Runtime  : 304 ms (beats 54%)
+-- Memory   : 0B (beats 100%)
+-- Language : mysql
+-- Copyright: (c) 2026 YathichaC. All rights reserved.
+-- Synced by: leetie
+-- ──────────────────────────────────────────────────
+
+# Write your MySQL query statement below
+SELECT MAX(salary) AS SecondHighestSalary FROM Employee
+WHERE salary < (
+    SELECT MAX(salary)
+    FROM Employee
+);
